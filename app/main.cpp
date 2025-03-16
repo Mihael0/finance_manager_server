b140a338826a38143bf2b4a586400a6b3adf27cb
CodeEnjoyer <mihael.nedyalkov@gmail.com>
2025-03-16 17:16:59 +0100

    Added libxl to the project

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "hellow.h"
 #include <boost/asio.hpp>
+#include <libxl.h>
 
 int main() {
     std::cout << "Hello World!\n";
     hello();
+
+    using namespace libxl;
+    Book* book = xlCreateBook(); // xlCreateXMLBook() for xlsx
+    if(book)
+    {
+        Sheet* sheet = book->addSheet("Sheet1");
+        if(sheet)
+        {
+            sheet->writeStr(2, 1, "Hello, World !");
+            sheet->writeNum(3, 1, 1000);
+        }
+        book->save("example.xls");
+        book->release();
+    }
     return 0;
 }
